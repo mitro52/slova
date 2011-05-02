42311ffe86f736f59c753c72278e6592ea0b56b4
mitro <mitro@mitro-laptop.(none)>
2011-05-02 15:36:10 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slova::Application.config.secret_token = '1410a564b143a7c982186214333126addc682d68c69e497039e1b0608b85e5ae17e0af4ef285e2ce7946e7aa983b2c42fad802c048198cbc62c1f9bd5146779e'
